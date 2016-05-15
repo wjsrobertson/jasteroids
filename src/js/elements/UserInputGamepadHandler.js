fdac4d1bf11c7aeaf007267c4de93a34c7f7e051
Will Robertson <wjsrobertson@gmail.com>
2016-05-15 22:05:49 +0100

    Tweak gamepad handling

--- src/js/elements/UserInputGamepadHandler.js
+++ src/js/elements/UserInputGamepadHandler.js
@@ -2,35 +2,79 @@ var Jasteroids = Jasteroids || {};
 
 Jasteroids.UserInputGamepadHandler = function (userInputProcessor) {
 
-    function handleGamepad(gamepad) {
-        if (gamepad.buttons[14].pressed || gamepad.axes[0] < -0.3) {
+    var axisThreshold = 0.3;
+
+    var buttons = {
+        left: [14],
+        right: [15],
+        up: [12],
+        down: [13],
+        shoot: [0, 2, 6, 7],
+        newGame: [9]
+    };
+
+    var axes = {
+        horizontal: [0],
+        vertical: [1]
+    };
+
+    function getGamepad() {
+        if (navigator.getGamepads() && navigator.getGamepads().length && navigator.getGamepads()[0]) {
+            return navigator.getGamepads()[0];
+        } else {
+            return null;
+        }
+    }
+
+    function isButtonPressed(buttonIds) {
+        var gamepad = getGamepad();
+        return buttonIds.some(function(buttonId) {
+            return gamepad && gamepad.buttons[buttonId] && gamepad.buttons[buttonId].pressed;
+        });
+    }
+
+    function isAxisNegative(axisIds) {
+        var gamepad = getGamepad();
+        return axisIds.some(function(axisId) {
+            return gamepad && gamepad.axes[axisId] < -axisThreshold;
+        });
+    }
+
+
+    function isAxisPositive(axisIds) {
+        var gamepad = getGamepad();
+        return axisIds.some(function(axisId) {
+            return gamepad && gamepad.axes[axisId] > axisThreshold;
+        });
+    }
+
+    function handleGamepad() {
+        if (isButtonPressed(buttons.left) || isAxisNegative(axes.horizontal)) {
             userInputProcessor.left();
-        } else if (gamepad.buttons[15].pressed || gamepad.axes[0] > 0.3) {
+        } else if (isButtonPressed(buttons.right)  || isAxisPositive(axes.horizontal)) {
             userInputProcessor.right();
         }
 
-        if (gamepad.buttons[12].pressed || gamepad.axes[1] < -0.3) {
+        if (isButtonPressed(buttons.up) || isAxisNegative(axes.vertical)) {
             userInputProcessor.forward();
-        } else if (gamepad.buttons[13].pressed || gamepad.axes[1] > 0.3) {
+        } else if (isButtonPressed(buttons.down) || isAxisPositive(axes.vertical)) {
             userInputProcessor.back();
         }
 
-        if (gamepad.buttons[0].pressed || gamepad.buttons[2].pressed || gamepad.buttons[6].pressed || gamepad.buttons[7].pressed) {
+        if (isButtonPressed(buttons.shoot)) {
             userInputProcessor.shoot();
         }
 
-        if (gamepad.buttons[9].pressed) {
+        if (isButtonPressed(buttons.newGame)) {
             userInputProcessor.newGame();
         }
     }
 
     return {
         tick: function() {
-            if (navigator.getGamepads() && navigator.getGamepads().length) {
-                var gamepad = navigator.getGamepads()[0];
-                handleGamepad(gamepad);
+            if (getGamepad()) {
+                handleGamepad();
             }
         }
     }
-
 };
